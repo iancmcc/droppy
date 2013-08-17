a19fc468c5eb86b8a7cb4a89679d1dcb3f331895
Ian McCracken <ian.mccracken@gmail.com>
2013-08-17 11:26:17 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -18,19 +18,19 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0'
+version = '0.1'
 
 setup(name='droppy',
       version=version,
-      description="",
+      description="Python framework for rapid service development",
       long_description="""\
 """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
-      author='',
-      author_email='',
-      url='',
-      license='',
+      author='Ian McCracken',
+      author_email='ian.mccracken@gmail.com',
+      url='http://github.com/iancmcc/droppy',
+      license='Apache License 2.0',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
@@ -40,7 +40,5 @@ setup(name='droppy',
           'pyxdeco'
       ],
       entry_points="""
-      [console_scripts]
-      doathing = droppy.application:main
       """,
       )
